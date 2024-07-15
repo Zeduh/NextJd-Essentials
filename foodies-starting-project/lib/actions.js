29f691dc1b338397ab9c9513853183619dab0a2a
zeduh <jose@setorinformatica.com>
2024-07-15 16:51:10 -0300

    Working with Server Action Responses & useFormState

--- foodies-starting-project/lib/actions.js
+++ foodies-starting-project/lib/actions.js
@@ -7,7 +7,7 @@ function isInvalidText(text) {
   return !text || text.trim() === '';
 }
 
-export async function shareMeal(formData) {
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -21,13 +21,14 @@ export async function shareMeal(formData) {
     isInvalidText(meal.title) ||
     isInvalidText(meal.summary) ||
     isInvalidText(meal.instructions) ||
-    isInvalidText(meal.image) ||
     isInvalidText(meal.creator) ||
     isInvalidText(meal.creator_email) ||
     !meal.creator_email.includes('@') ||
     !meal.image || meal.image.size === 0
   ){
-    throw new Error('Invalid input');
+    return {
+      message: 'Invalid input.',
+    }
   }
 
   await saveMeal(meal);
